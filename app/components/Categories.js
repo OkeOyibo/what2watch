15b4400d778b94bd88ec9a5cdeb1f3116252fc9f
Severin Hasler <severinhasler@hotmail.com>
2021-01-14 08:23:35 +0100

    added some folders / destructering

---
+++ app/components/Categories.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+
+function Categories (props) {
+    return(
+        <View>
+            <Text> New View </Text>
+        </View>
+    )  
+}
+
+export default Categories;
