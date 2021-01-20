eb76e0412c25c7e131edd4a4ae09f26a1dabfc8f
Severin Hasler <severinhasler@hotmail.com>
2021-01-20 17:42:10 +0100

    created

---
+++ app/screens/Selection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Selection(props) {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default Selection;
