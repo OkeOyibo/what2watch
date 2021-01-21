6b1eda3afa2f8bb43c5de0bbe0d6cb49226514fc
Severin Hasler <severinhasler@hotmail.com>
2021-01-21 10:55:55 +0100

    added base funcionality

--- app/screens/Selection.js
+++ app/screens/Selection.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Button, Icon, Text } from 'react-native-elements';
+import MyCarouel from '../components/MyCarousel';
 
-function Selection(props) {
+function Selection({ navigation }) {
     return (
-        <View>
-            
-        </View>
+        <SafeAreaView style={styles.wrapper}>
+            <View style={styles.header}>
+                <Button
+                    title={'go back'}
+                    titleStyle={{ color: 'white' }}
+                    buttonStyle={{ backgroundColor: 'black' }}
+                    icon={<Icon name={'navigate-before'} color={'white'} />}
+                    onPress={() => {navigation.goBack()}}
+                />
+            </View>
+            <MyCarouel />
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        backgroundColor: 'black',
+    },
+    header: {
+        marginTop: 20,
+        flexDirection: "row",
+        alignItems: "flex-start"
+    }
+})
 
 export default Selection;
