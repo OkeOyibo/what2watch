cd1bd1f30bd3c236c9ef6f3ce724fc5c512399b0
Severin Hasler <severinhasler@hotmail.com>
2021-01-21 20:03:56 +0100

    bug fix

--- app/components/MyCarousel.js
+++ app/components/MyCarousel.js
@@ -1,16 +1,20 @@
 import * as React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import Carousel from 'react-native-snap-carousel-chen';
+import { View, Dimensions, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import Carousel, { ParallaxImage } from 'react-native-snap-carousel-chen';
+import { discoverMovies } from '../services/MovieService';
+
+const { width: screenWidth } = Dimensions.get('window')
 
 export default class MyCarousel extends React.Component {
-
     constructor(props){
         super(props);
         this.state = {
           activeIndex:0,
+          data: [],
           carouselItems: [
           {
-              title:"Item 1",
+              title:"Goku: Midnight Eye",
               text: "Text 1",
           },
           {
@@ -32,38 +36,96 @@ export default class MyCarousel extends React.Component {
         ]
       }
     }
+    
+    componentDidMount() {
+        const genres = this.props.genres;
+        let response = [];
+        let results = [];
 
-    _renderItem({item,index}){
+        const instance = this;
+
+        function setData(values) {
+            instance.setState({data: values})
+        };
+
+        async function fetchdata() {
+            try {
+                response = await discoverMovies(genres);
+                results = await response.results;
+
+                setData(results);
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        if (!this.state.data.length > 0) {
+            fetchdata();
+        }
+    }
+
+    _renderItem({item, index}, parallaxProps){
         return (
-          <View style={{
-              backgroundColor:'floralwhite',
-              borderRadius: 5,
-              height: 250,
-              padding: 50,
-              marginLeft: 25,
-              marginRight: 25, }}>
-            <Text style={{fontSize: 30}}>{item.title}</Text>
-            <Text>{item.text}</Text>
-          </View>
+            <View style={styles.item}>
+                <ParallaxImage
+                    source={{ uri: 'https://image.tmdb.org/t/p/w500/' + item.thumbnail }}
+                    containerStyle={styles.imageContainer}
+                    style={styles.image}
+                    parallaxFactor={0.4}
+                    {...parallaxProps}
+                />
+                <Text h4 style={styles.title} numberOfLines={2}>
+                    { item.title }
+                </Text>
+            </View>
 
         )
     }
 
     render() {
         return(
-            <View style={{flex: 1, backgroundColor:'black', paddingTop: 50, }}>
-                <View style={{ flex: 1, flexDirection:'column', justifyContent: 'center', marginTop: 100, alignItems: 'center' }}>
-                    <Carousel
-                    layout={'stack'}
-                    layoutCardOffset={9}
-                    ref={ref => this.carousel = ref}
-                    data={this.state.carouselItems}
-                    sliderWidth={300}
-                    itemWidth={250}
+            <View style={styles.container}>
+                <Carousel
+                    sliderWidth={250}
+                    sliderHeight={400}
+                    itemWidth={200}
+                    data={
+                        this.state.data.map((item) => {
+                            return {
+                                title: item.title,
+                                thumbnail: item.poster_path
+                            }})
+                    }
                     renderItem={this._renderItem}
-                    onSnapToItem = { index => this.setState({activeIndex:index}) } />
-                </View>
-          </View>
+                    hasParallaxImages={true}
+                />
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection:'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: screenWidth / 4
+    },
+    item: {
+        width: 200,
+        height: 400,
+    },
+    imageContainer: {
+      flex: 1,
+      marginBottom: Platform.select({ ios: 0, android: 1 }), // Prevent a random Android rendering issue
+      backgroundColor: 'black',
+      borderRadius: 8,
+    },
+    image: {
+      ...StyleSheet.absoluteFillObject,
+      resizeMode: 'contain',
+    },
+    title: {
+        color: 'white',
+    }
+  })
